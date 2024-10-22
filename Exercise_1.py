2a4cf09a2a1fc6907f702b1cbd6014b17f4c2ba6
sejalrj <sj3251.nyu.edu>
2024-10-22 12:39:42 -0500

    completed Pre1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,24 +1,41 @@
+# Time Complexity : for each operation, O(1)
+# Space Complexity : O(N) where N is the maximum length of integer inputs
+
 class myStack:
+
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
-     def isEmpty(self):
-         
-     def push(self, item):
-         
-     def pop(self):
+  def __init__(self):
+    self.stack = []
+
+  def isEmpty(self):
+    if len(self.stack) != 0:
+      return True
+    return False
         
+  def push(self, item):
+    self.stack.append(item)
+    return
         
-     def peek(self):
-        
-     def size(self):
-         
-     def show(self):
+
+  def pop(self):
+    return self.stack.pop()
+      
+  def peek(self):
+    if self.stack:
+      return self.stack[-1]
+    
+  def size(self):
+    return len(self.stack)
+      
+  def show(self):
+    return self.stack
          
 
 s = myStack()
 s.push('1')
 s.push('2')
+print(s.peek())
+print(s.isEmpty())
 print(s.pop())
 print(s.show())
